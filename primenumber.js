763188046ede69dd9efd2f78aedb1838d53d4417
omkarSw2 <119350956+omkarSw2@users.noreply.github.com>
2022-12-01 13:43:06 +0530

    Update primenumber.js
    done

--- primenumber.js
+++ primenumber.js
@@ -1,8 +1,13 @@
-let num=1221;
-let count=0;
-    
-    for(let i=1;i<=num;i--){if(num%i==0&&num%num==0)
-    {count++;
-    }}
-        if(count==2){console.log("Yes")}
-        else{console.log("No")}
+let num=13;
+    let count=0;
+    for(i=0; i<=num; i++){
+        if(num%i==0 ){
+            count++
+        }
+    }
+
+if(count==2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
